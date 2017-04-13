14e8618e4fd98373d509aff08566ff9aafbd13df
sweezy974 <sweezy974@live.fr>
2017-04-13 23:35:43 +0400

    test reservation

--- test/tavoituretest.php
+++ test/tavoituretest.php
@@ -75,32 +75,48 @@ class DbTest extends TestCase{
          $this->assertEquals($expectedTable, $result);
    }
 
- //   public function testDisplayReservation()
- //   {
- //
- //       $database = $this->getConnection();
- //       $queryTable = $database->prepare(
- //          'SELECT * FROM vehicle WHERE available="0" ');
- //       $result = $queryTable->execute();
- //       $result= $queryTable->fetchAll(PDO::FETCH_ASSOC);
- //       $expectedTable = [
- //          ["id" => "2",
- //           "price" => "80.00",
- //           "places" =>"2",
- //           'places' => '2',
- //     'color' => 'rouge',
- //     'user_id' => '1',
- //     'model_id' => '8',
- //     'address_id' => '1',
- //     'fuel' => 'essence',
- //     'status' => 'bon etat',
- //      'description' => 'peinture fresh',
- //      'available' => '0'
- //
- //        ]
- //    ];
- //
- //       $this->assertEquals($expectedTable, $result);
- // }
+   public function testDisplayAcceptedReservation()
+   {
+
+       $database = $this->getConnection();
+       $queryTable = $database->prepare(
+          'SELECT * FROM reservation WHERE status="1" ');
+       $result = $queryTable->execute();
+       $result= $queryTable->fetchAll(PDO::FETCH_ASSOC);
+       $expectedTable = [
+          ["id" => "1",
+          'status' => '1',
+          'vehicle_id' => '1',
+          'user_id' => '1',
+          'start_booking' => '2017-04-13 00:00:00',
+          'end_booking' => '2017-04-17 00:00:00'
+
+       ]
+    ];
+
+       $this->assertEquals($expectedTable, $result);
+ }
+
+ public function testDisplayReservationSend()
+{
+
+     $database = $this->getConnection();
+     $queryTable = $database->prepare(
+        'SELECT * FROM reservation WHERE status="0" ');
+     $result = $queryTable->execute();
+     $result= $queryTable->fetchAll(PDO::FETCH_ASSOC);
+     $expectedTable = [
+        ["id" => "2",
+        'status' => '0',
+        'vehicle_id' => '1',
+        'user_id' => '1',
+        'start_booking' => '2017-04-24 00:00:00',
+        'end_booking' => '2017-04-30 00:00:00'
+
+     ]
+  ];
+
+     $this->assertEquals($expectedTable, $result);
+}
 }
 ?>
